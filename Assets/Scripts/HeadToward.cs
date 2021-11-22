c8523368921543a3079bf67d7451a4b9694fe86a
lemomade <re991027@163.com>
2021-11-22 22:24:57 +0800

    场景还没弄

---
+++ Assets/Scripts/HeadToward.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class HeadToward : MonoBehaviour
+{
+    public Material FaceMateria;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void SetHeadDirection()
+    {
+        if(this.FaceMateria != null)
+        {
+            this.FaceMateria.SetVector("_HeadForward1", this.transform.forward);
+            this.FaceMateria.SetVector("_HeadRight1", this.transform.right);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.SetHeadDirection();
+    }
+}
---
+++ Assets/retarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class retarget : MonoBehaviour
+{
+    
+    public GameObject son;
+    public GameObject father;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Dir = -son.transform.position + father.transform.position;
+        //float angle = Vector3.Angle(-transform.forward,Dir); 
+
+        //transform.localEulerAngles = new Vector3(-angle, 0, 0);
+       // transform.rotation=Quaternion.Euler(-angle,0.0f,0.0f);
+       // transform.LookAt(father.transform.position);
+        transform.up =  Dir ;
+         //transform.forward  =new Vector3(6, 0, 0);
+        
+        
+    }
+}
